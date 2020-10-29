ffd71614dfadc9973f6cfe010255b2665b484443
matigrzegor <matigrzegor@gmail.com>
2020-10-29 13:33:31 +0100

    Add redux, add basic redux setup in index.js, class components and store/reducer.js

--- src/containers/motorcycles-list.js
+++ src/containers/motorcycles-list.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import "bootstrap/dist/css/bootstrap.min.css";
 import MotorcycleDataService from "./../services/motorcycle.service";
 
